292036f1d76238b069ecdb2e0fbada52a79921ea
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-04-20 01:58:12 -0300

    imports

--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -3,12 +3,18 @@ import 'assets/sass/app.scss';
 import Vue from 'vue';
 import Electron from 'vue-electron';
 import Router from 'vue-router';
+
 import Giphy from 'core/giphy';
+window.Giphy = Giphy;
+
+import Bus from 'core/bus';
+window.Bus = new Bus(Vue);
+
+import 'core/directives';
+import 'core/components';
 
 import App from './App';
 import routes from 'core/routes';
-
-window.Giphy = Giphy;
 
 Vue.use(Electron);
 Vue.use(Router);
