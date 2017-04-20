35660acde63f84dbe206205bcbd279661474d7b6
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-04-19 23:06:45 -0300

    axios

--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -1,21 +1,23 @@
-import Vue from 'vue'
-import Electron from 'vue-electron'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Electron from 'vue-electron';
+import Router from 'vue-router';
+import Axios from 'axios';
 
-import App from './App'
-import routes from './routes'
+import App from './App';
+import routes from './routes';
 
-Vue.use(Electron)
-Vue.use(Router)
-Vue.config.debug = true
+window.Axios = Axios;
+
+Vue.use(Electron);
+Vue.use(Router);
+Vue.config.debug = true;
 
 const router = new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes
-})
+});
 
-/* eslint-disable no-new */
 new Vue({
   router,
   ...App
-}).$mount('#app')
+}).$mount('#app');
