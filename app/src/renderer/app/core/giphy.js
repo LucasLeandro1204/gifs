9f3194994bb8ab1cffe22e974c59b45fdd5afa36
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-04-22 20:28:07 -0300

    offset

--- app/src/renderer/app/core/giphy.js
+++ app/src/renderer/app/core/giphy.js
@@ -2,26 +2,17 @@ import Axios from 'axios';
 Axios.defaults.baseURL = 'http://api.giphy.com/v1/gifs/';
 
 export default {
-  search (query, page = 0) {
+  search (query, offset = 0) {
     return new Promise((resolve, reject) => {
       Axios.get('search', {
         params: {
           q: query,
-          offset: this.page(page),
-          api_key: 'dc6zaTOxFJmzC',
-          onDownloadProgress (progressEvent) {
-            console.log('kkk');
-            let percentCompleted = Math.floor((progressEvent.loaded * 100) / progressEvent.total);
-            console.log(percentCompleted);
-          }
+          offset: offset,
+          api_key: 'dc6zaTOxFJmzC'
         }
       })
       .then(response => resolve(response))
       .catch(error => reject(error));
     });
-  },
-
-  page (n) {
-    return n * 25;
   }
 };
